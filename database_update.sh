848d6c224d00457f360ab6e04923ef7f930d0142
Marcin Cholewka <Marcin@DESKTOP-QC1DUAN>
2022-12-10 17:23:49 +0100

    Update scripts after project rename

--- database_update.sh
+++ database_update.sh
@@ -1,1 +1,1 @@
- dotnet ef --project Polls.Infrastructure/Polls.Infrastructure.csproj --startup-project Polls.Service/Polls.Service.csproj database update --context SqliteDbContext --verbose
+ dotnet ef --project Gymmer.Infrastructure/Gymmer.Infrastructure.csproj --startup-project Gymmer.Service/Gymmer.Service.csproj database update --context SqliteDbContext --verbose
--- run_migrations.sh
+++ run_migrations.sh
@@ -1,1 +1,1 @@
-dotnet ef --project Polls.Infrastructure/Polls.Infrastructure.csproj --startup-project Polls.Service/Polls.Service.csproj migrations add InitialCreate --context SqliteDbContext --output-dir ./Persistence/SqliteMigrations --verbose
+dotnet ef --project Gymmer.Infrastructure/Gymmer.Infrastructure.csproj --startup-project Gymmer.Service/Gymmer.Service.csproj migrations add InitialCreate --context SqliteDbContext --output-dir ./Persistence/SqliteMigrations --verbose
