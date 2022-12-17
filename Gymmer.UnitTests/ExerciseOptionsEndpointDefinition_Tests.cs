e2ca4da1ffff22e5a5703f116ab93e08ae952486
Plusce <marcin.cholewka12@gmail.com>
2022-12-17 15:21:30 +0100

    Add unit tests for ExerciseOptions API endpoints

--- Gymmer.UnitTests/ExerciseOptionsEndpointDefinition_Tests.cs
+++ Gymmer.UnitTests/ExerciseOptionsEndpointDefinition_Tests.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using NSubstitute;
 using Gymmer.Infrastructure.Persistence.Models;
 using Gymmer.Infrastructure.Persistence.Repository;
+using NSubstitute.ReturnsExtensions;
 using Xunit;
 
 namespace Gymmer.UnitTests;
@@ -14,13 +15,13 @@ public class ExerciseOptionsEndpointDefinition_Tests
         Substitute.For<IExerciseOptionsRepository>();
 
     [Fact]
-    public async Task FindAll_ReturnAll()
+    public async Task Get_FindAllExerciseOptions_Successfully()
     {
         // Arrange
         _exerciseOptionsRepository.FindAllAsync().ReturnsForAnyArgs(new List<ExerciseOptionModel?>
         {
-            new (){ Name = "Zakroki", Description = "Spokojne tempo do tyłu i do przodu" },
-            new (){ Name = "Wyciskanie bokiem", Description = "Ćwiczenie w celu otwierania 2x w tygodniu" }
+            new() { Name = "Zakroki", Description = "Spokojne tempo do tyłu i do przodu" },
+            new() { Name = "Wyciskanie bokiem", Description = "Ćwiczenie w celu otwierania 2x w tygodniu" }
         });
 
         // Act
@@ -31,5 +32,62 @@ public class ExerciseOptionsEndpointDefinition_Tests
         result.As<Ok<List<string?>?>>().Value.Should()
             .BeEquivalentTo("Zakroki", "Wyciskanie bokiem");
         result.As<Ok<List<string?>?>>().StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public async Task Post_AddNewExerciseOption_Successfully()
+    {
+        // Arrange
+        _exerciseOptionsRepository
+            .AddAsync(Substitute.For<ExerciseOptionModel>(), CancellationToken.None)
+            .ReturnsNull();
+
+        // Act
+        var result =
+            await ExerciseOptionsApiQueries.Post(new PostExerciseOptionCommand(),
+                _exerciseOptionsRepository,
+                CancellationToken.None);
+
+        // Assert
+        result.As<Created>().StatusCode.Should().Be(201);
+    }
+
+    [Fact]
+    public async Task Put_UpdateExerciseOption_Successfully()
+    {
+        // Arrange
+        _exerciseOptionsRepository
+            .FindByIdAsync(Arg.Any<long>(), CancellationToken.None)
+            .Returns(new ExerciseOptionModel());
+        _exerciseOptionsRepository
+            .UpdateAsync(Substitute.For<ExerciseOptionModel>(), CancellationToken.None)
+            .ReturnsNull();
+
+        // Act
+        var result =
+            await ExerciseOptionsApiQueries.Put(new PutExerciseOptionCommand(),
+                _exerciseOptionsRepository,
+                CancellationToken.None);
+
+        // Assert
+        result.As<Accepted>().StatusCode.Should().Be(202);
+    }
+    
+    [Fact]
+    public async Task Delete_RemoveExerciseOption_Successfully()
+    {
+        // Arrange
+        _exerciseOptionsRepository
+            .RemoveAsync(Arg.Any<long>(), CancellationToken.None)
+            .ReturnsNull();
+
+        // Act
+        var result =
+            await ExerciseOptionsApiQueries.Delete(Arg.Any<long>(),
+                _exerciseOptionsRepository,
+                CancellationToken.None);
+
+        // Assert
+        result.As<NoContent>().StatusCode.Should().Be(204);
     }
 }
