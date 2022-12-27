7b6f99838f17ce23e152ce01e5ea3dc9734b7892
Plusce <marcin.cholewka12@gmail.com>
2022-12-27 12:36:19 +0100

    Add missing migrations

---
+++ Gymmer.Infrastructure/Persistence/SqliteMigrations/20221227110602_DescriptionToTrainingDefinition.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gymmer.Infrastructure.Persistence.SqliteMigrations
+{
+    /// <inheritdoc />
+    public partial class DescriptionToTrainingDefinition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "TrainingDefinition",
+                type: "TEXT",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "TrainingDefinition");
+        }
+    }
+}
